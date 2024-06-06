16bf8d478a0330cd3110414e56fbd8cfd33abfb7
Reinier de Armas <reinier.dearmas@gmail.com>
2024-06-06 00:15:48 -0400

    Commit RLA-7 Implementacion Validacion Email y Contraseñas

---
+++ app/src/main/java/com/example/refactoringlifeacademy/viewmodels/RegisterViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.refactoringlifeacademy.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import com.example.refactoringlifeacademy.utils.StringUtils.isValidEmail
+import com.example.refactoringlifeacademy.utils.StringUtils.isValidPassword
+
+class RegisterViewModel : ViewModel() {
+    private val _isFormValid = MutableLiveData<Boolean>()
+    val isFormValid: LiveData<Boolean> = _isFormValid
+
+    // Funcion que valida email
+    fun validateEmail(email: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val isEmailValid = email.isValidEmail()
+            updateFormValidity(isEmailValid, true, true)
+        }
+    }
+
+    // Funcion que valida contraseña
+    fun validatePassword(password: String, option: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val isPasswordValid = password.isValidPassword()
+            when (option) {
+                0 ->
+                    updateFormValidity(true, isPasswordValid, true)
+
+                1 ->
+                    updateFormValidity(true, true, isPasswordValid)
+            }
+        }
+    }
+
+    // Funcion que actualiza el valor del LiveData
+    private fun updateFormValidity(
+        isEmailValid: Boolean,
+        isPasswordValid: Boolean,
+        isPasswordConfValid: Boolean
+    ) {
+        _isFormValid.postValue(isEmailValid && isPasswordValid && isPasswordConfValid)
+    }
+}
