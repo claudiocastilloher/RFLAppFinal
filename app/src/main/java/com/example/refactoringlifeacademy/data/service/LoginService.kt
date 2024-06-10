08be443536180955637d1456ae2bd87d18b09bfb
Eduardo <eduardobasilio30@gmail.com>
2024-06-10 16:26:25 -0600

    Feature/RLA-8 conección del login con API

---
+++ app/src/main/java/com/example/refactoringlifeacademy/data/service/LoginService.kt
@@ -0,0 +1,12 @@
+package com.example.refactoringlifeacademy.data.service
+
+import com.example.refactoringlifeacademy.data.dto.request.LoginRequest
+import com.example.refactoringlifeacademy.data.dto.response.LoginResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface LoginService {
+    @POST("api/v1/auth/login")
+    fun loginUser(@Body request: LoginRequest): Response<LoginResponse>
+}
