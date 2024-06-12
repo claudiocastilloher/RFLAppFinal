4ef3e49fea77de1c1561ea6d51936364e03e35f3
Reinier de Armas <reinier.dearmas@gmail.com>
2024-06-12 00:57:23 -0400

    Commit API

---
+++ app/src/main/java/com/example/refactoringlifeacademy/data/service/RegisterService.kt
@@ -0,0 +1,12 @@
+package com.example.refactoringlifeacademy.data.service
+
+import com.example.refactoringlifeacademy.data.dto.request.RegisterRequest
+import com.example.refactoringlifeacademy.data.dto.response.RegisterResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface RegisterService {
+    @POST("api/v1/auth/login")
+    fun registerUser(@Body request: RegisterRequest): Response<RegisterResponse>
+}
---
+++ app/src/main/java/com/example/refactoringlifeacademy/data/service/RegisterServiceImplement.kt
@@ -0,0 +1,21 @@
+package com.example.refactoringlifeacademy.data.service
+
+import com.example.refactoringlifeacademy.data.dto.request.RegisterRequest
+import com.example.refactoringlifeacademy.data.dto.response.RegisterResponse
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class RegisterServiceImplement {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api-users-c9xg.onrender.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val service = retrofit.create<RegisterService>()
+
+    fun registerUser(request: RegisterRequest): Response<RegisterResponse> {
+        return service.registerUser(request)
+    }
+}
