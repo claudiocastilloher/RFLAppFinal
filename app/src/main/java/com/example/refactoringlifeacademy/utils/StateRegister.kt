ff9e64c244e01bb17ebe1cf08660afe78d00636e
Emilce Cano <emilce.cano95@gmail.com>
2024-06-19 23:47:03 -0300

    viendo que erro tengo

--- app/src/main/java/com/example/refactoringlifeacademy/utils/StateRegister.kt
+++ app/src/main/java/com/example/refactoringlifeacademy/utils/StateRegister.kt
@@ -4,7 +4,7 @@ import com.example.refactoringlifeacademy.data.dto.response.LoginResponse
 
 sealed class StateRegister {
 
-    data class Succes(private val info:String): StateRegister()
+    data class Success(private val info:String): StateRegister()
     data class Error(val message: String): StateRegister()
     data object Loading : StateRegister()
 
