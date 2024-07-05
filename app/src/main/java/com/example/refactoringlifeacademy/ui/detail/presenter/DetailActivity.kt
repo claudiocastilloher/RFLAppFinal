2b84c828db26d22cd43dba08402ac743da1b26ab
Reinier de Armas <reinier.dearmas@gmail.com>
2024-07-05 02:42:50 -0400

    Commit 3 Carga de los Fragments desde la Activity

--- app/src/main/java/com/example/refactoringlifeacademy/ui/detail/presenter/DetailActivity.kt
+++ app/src/main/java/com/example/refactoringlifeacademy/ui/detail/presenter/DetailActivity.kt
@@ -1,8 +1,11 @@
 package com.example.refactoringlifeacademy.ui.detail.presenter
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.refactoringlifeacademy.R
 import com.example.refactoringlifeacademy.databinding.ActivityDetailBinding
+import com.example.refactoringlifeacademy.ui.home.presenter.HomeActivity
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
@@ -10,5 +13,35 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.tvImag.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ImageFragment())
+                .commit()
+        }
+
+        binding.tvDescript.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, DescriptionFragment())
+                .commit()
+        }
+
+        binding.tvFinanc.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FinanceFragment())
+                .commit()
+        }
+
+        binding.tvComent.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, CommentFragment())
+                .commit()
+        }
+
+        binding.arrowLeft1.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
