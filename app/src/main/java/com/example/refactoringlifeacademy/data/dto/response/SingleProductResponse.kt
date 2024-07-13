25aed62845fcdc47c4308bf187cbbbbbc6c6c358
Eduardo <eduardobasilio30@gmail.com>
2024-07-13 17:50:32 -0600

    RLA-05

--- app/src/main/java/com/example/refactoringlifeacademy/data/dto/response/SingleProductResponse.kt
+++ app/src/main/java/com/example/refactoringlifeacademy/data/dto/response/SingleProductResponse.kt
@@ -1,7 +1,6 @@
 package com.example.refactoringlifeacademy.data.dto.response
 
 import com.example.refactoringlifeacademy.data.dto.model.Product
-import com.google.gson.annotations.SerializedName
 
 data class SingleProductResponse(
     val product: Product?
