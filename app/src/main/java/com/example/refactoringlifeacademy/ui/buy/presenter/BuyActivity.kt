807dee9287833939503ffbdc60ca6af74e5a4f4e
Reinier de Armas <reinier.dearmas@gmail.com>
2024-07-13 03:56:53 -0400

    Commit 1 para PR RLA-18

---
+++ app/src/main/java/com/example/refactoringlifeacademy/ui/buy/presenter/BuyActivity.kt
@@ -0,0 +1,21 @@
+package com.example.refactoringlifeacademy.ui.buy.presenter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.refactoringlifeacademy.R
+
+class BuyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_buy)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
