be7930b412ccb389101f80b8241158e590baafb3
Reinier de Armas <reinier.dearmas@gmail.com>
2024-07-14 20:48:07 -0400

    Commit Merge cambios HomeActivity

--- app/src/main/java/com/example/refactoringlifeacademy/ui/buy/presenter/BuyActivity.kt
+++ app/src/main/java/com/example/refactoringlifeacademy/ui/buy/presenter/BuyActivity.kt
@@ -1,21 +1,15 @@
 package com.example.refactoringlifeacademy.ui.buy.presenter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.refactoringlifeacademy.R
+import com.example.refactoringlifeacademy.databinding.ActivityBuyBinding
 
 class BuyActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBuyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_buy)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityBuyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/refactoringlifeacademy/ui/home/presenter/HomeActivity.kt
+++ app/src/main/java/com/example/refactoringlifeacademy/ui/home/presenter/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.refactoringlifeacademy.ui.home.presenter
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
